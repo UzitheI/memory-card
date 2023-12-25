0bbab4926a7b012c1e3f186a2896e299fce92131
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-25 21:13:35 +0545

    new changes

---
+++ project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App(){
+    return(
+        <div className='w-max mx-auto my-30'>
+            <h1>Magic Match</h1>
+            <button className='bg-none border border-red-200 px-6 py-12 rounded-lg text-red-300 font-bold cursor-pointer text-2 hover:bg-red-600 hover:text-red-100'>New Game</button>
+        </div>
+    );
+}
--- project/src/playAudio.js
+++
@@ -1,4 +0,0 @@
-export default function playAudio(audio){
-    audio.currentTime=0;
-    audio.play();
-}
